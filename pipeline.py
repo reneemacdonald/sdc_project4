4b93f8b2c954b105f1214d2bfdc82da4ead41980
Renee MacDonald <rm51@users.noreply.github.com>
2017-09-06 20:41:35 -0700

    added grayscale
    canny and gaussian noise

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,31 @@ import cv2
 
 clip1= cv2.VideoCapture('project_video.mp4')
 
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    you should call plt.imshow(gray, cmap='gray')"""
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale conversion
+    return gray
+
+def gaussian_noise(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+
+
+    # Define a kernel size for Gaussian smoothing / blurring
+    kernel_size = 5
+    blur_gray = gaussian_noise(img, kernel_size)
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    return cv2.Canny(img, low_threshold, high_threshold)
+
 
 def process_video(clip1):
 
@@ -11,8 +36,10 @@ def process_video(clip1):
 	while (clip1.isOpened()):
 		ret, frame = clip1.read()
 
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		cv2.imshow('frame', gray)
+		grayscale_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		edges = canny(grayscale_image, 50, 150)
+
+		cv2.imshow('frame', edges)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
