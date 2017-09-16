42e827c249be939c92296c4ffb9003c04852b401
Renee MacDonald <rm51@users.noreply.github.com>
2017-09-15 22:00:53 -0700

    measuring curvature
    But using the stock curve rather than the actual images

--- pipeline.py
+++ pipeline.py
@@ -197,6 +197,7 @@ def find_the_lines(binary_warped):
 	plt.ylim(720, 0)
 
 	plt.show()
+	measuring_curvature()
 
 def camera_calibration(images):
 	# Arrays to store object points and image points from all the images
@@ -246,6 +247,48 @@ def undistortion(objpoints, imgpoints, img, image_name):
 	plt.show()
 	#ax2.set_title('Undistorted Image', fontsize=30)
 
+def measuring_curvature():
+	# Generate some fake data to represent lane-line pixels
+	ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
+	quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+	# For each y position generate random x position within +/-50 pix
+	# of the line base position in each case (x=200 for left, and x=900 for right)
+	leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+	                              for y in ploty])
+	rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+	                                for y in ploty])
+
+	leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+	rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+
+	# Fit a second order polynomial to pixel positions in each fake lane line
+	left_fit = np.polyfit(ploty, leftx, 2)
+	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+	right_fit = np.polyfit(ploty, rightx, 2)
+	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+	# Plot up the fake data	
+	mark_size = 3
+	plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+	plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
+	plt.xlim(0, 1280)
+	plt.ylim(0, 720)
+	plt.plot(left_fitx, ploty, color='green', linewidth=3)
+	plt.plot(right_fitx, ploty, color='green', linewidth=3)
+	plt.gca().invert_yaxis() # to visualize as we do the images
+	plt.show()
+
+	# Define y-value where we want radius of curvature
+	# I'll choose the maximum y-value, corresponding to the bottom of the image
+	y_eval = np.max(ploty)
+	left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+	right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+	print(left_curverad, right_curverad)
+
+
+# Example values: 1926.74 1908.48
+
 #commenting out because testing out other areas
 '''
 image_array = []
@@ -281,6 +324,7 @@ for lines_image in lines_images:
 	#plt.show()
 
 process_video(lines_array)
+
 #find_the_lines(color_and_gradient)
 #exit()
 '''
