7811f750d8fc5d8449b051bcf95be4b840b9ec29
Renee MacDonald <rm51@users.noreply.github.com>
2017-09-18 20:56:04 -0700

    calculating the radius of curvature
    Appears an acceptable value but differing values

--- pipeline.py
+++ pipeline.py
@@ -289,6 +289,20 @@ def measuring_curvature(original_image, warped, Minv):
 	right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
 	print("left curved", left_curverad, right_curverad)
 
+	# Define conversions in x and y from pixels space to meters
+	ym_per_pix = 30/720 # meters per pixel in y dimension
+	xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+	# Fit new polynomials to x,y in world space
+	left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+	right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+	# Calculate the new radii of curvature
+	left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+	right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+	# Now our radius of curvature is in meters
+	print(left_curverad, 'm', right_curverad, 'm')
+	# Example values: 632.1 m    626.2 m
+
 	# Create an image to draw the lines on
 	#warp_zero = np.zeros_like(warped).astype(np.uint8)
 	warp_zero = np.zeros_like(warped)
